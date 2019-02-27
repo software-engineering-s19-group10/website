d9ed1115f73ac9292493403a9da758dcf8868a04
DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>
2019-02-26 20:05:21 -0500

    Add section buttons

---
+++ app.js
@@ -0,0 +1,24 @@
+const sectionNames = ["home", "about"];
+const sections = {}, sectionButtons = {};
+
+sectionNames.forEach(function(name){
+    console.log(name);
+    sections[name] = document.getElementById("section-" + name);
+    sectionButtons[name] = document.getElementById("button-" + name);
+});
+
+const showSection = function(sectionName)
+{
+    for (let section in sections) {
+        if (section == sectionName)
+            sections[section].classList.remove("hidden-section");
+
+        else
+            sections[section].classList.add("hidden-section");
+    }
+}
+
+for (let button in sectionButtons) {
+    console.log(button);
+    sectionButtons[button].onclick = function() { showSection(button); console.log("Hello World") }
+}
